0abcc4dffe7c0c509f2919249ab5a1f26b4b08a0
Otabekrasuljonov2006 <rasuljonovotabek45@gmail.com>
2026-03-04 11:59:27 +0500

    Update websocket URL pattern to allow room names without slashes

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    # \w+ o'rniga [^/]+ qo'shildi
+    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
